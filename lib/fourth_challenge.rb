f7b1f2d45ffa37bebb27ab565a1cb32848d12f1f
Andrew J Casarsa <acasarsa@gmail.com>
2020-02-01 22:53:47 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,14 +5,6 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        #your hashes here!
-      ]
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
         {name: "Benvolio",
         age: "17",
         attitude: "worried"
@@ -21,6 +13,14 @@ def fourth_challenge
         age: "18",
         attitude: "hot-headed"
       }
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+
       ]
    }
 }
